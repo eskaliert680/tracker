029c7d27a0ae1f8e7961b46627b8bb50cc586aeb
gray <115596729+eskaliert680@users.noreply.github.com>
2025-03-26 18:03:11 -0700

    first commit
    ran cargo init, starting work on readme

---
+++ target/debug/deps/src-45aaf6c218aa0ff3.d
@@ -0,0 +1,5 @@
+/home/gray/Documents/GitHub/tracker/target/debug/deps/libsrc-45aaf6c218aa0ff3.rmeta: src/main.rs
+
+/home/gray/Documents/GitHub/tracker/target/debug/deps/src-45aaf6c218aa0ff3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/src-f2ee5684ce47b912.d
@@ -0,0 +1,5 @@
+/home/gray/Documents/GitHub/tracker/target/debug/deps/libsrc-f2ee5684ce47b912.rmeta: src/main.rs
+
+/home/gray/Documents/GitHub/tracker/target/debug/deps/src-f2ee5684ce47b912.d: src/main.rs
+
+src/main.rs:
